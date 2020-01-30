954348e9d18b65a82edc889b3497a3d9dc771f28
pepe_fabra_valverde <jofaval@outlook.com>
2020-01-30 23:43:27 +0100

    Fixed remove text button on inputs

--- scripts/js/inputs.js
+++ scripts/js/inputs.js
@@ -1,39 +1,48 @@
 var typingTimer;
 var usernameInputDoneTypingInterval = 5000;
-var $input = $('#myInput');
+var $input = $("#myInput");
 
-$input.on('keyup', function() {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(usernameInputDoneTyping, usernameInputDoneTypingInterval);
+$input.on("keyup", function() {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(
+    usernameInputDoneTyping,
+    usernameInputDoneTypingInterval
+  );
 });
 
-$input.on('keydown', function() {
-    clearTimeout(typingTimer);
+$input.on("keydown", function() {
+  clearTimeout(typingTimer);
 });
 
 function usernameInputDoneTyping() {
-    //do something
+  //do something
 }
 
-$("#show_hide_password a").on('click', function(event) {
-    event.preventDefault();
-    if ($('#show_hide_password input').attr("type") == "text") {
-        $('#show_hide_password input').attr('type', 'password');
-        $('#show_hide_password i').addClass("fa-eye-slash");
-        $('#show_hide_password i').removeClass("fa-eye");
-    } else if ($('#show_hide_password input').attr("type") == "password") {
-        $('#show_hide_password input').attr('type', 'text');
-        $('#show_hide_password i').removeClass("fa-eye-slash");
-        $('#show_hide_password i').addClass("fa-eye");
-    }
+$("#show_hide_password a").on("click", function(event) {
+  event.preventDefault();
+  if ($("#show_hide_password input").attr("type") == "text") {
+    $("#show_hide_password input").attr("type", "password");
+    $("#show_hide_password i").addClass("fa-eye-slash");
+    $("#show_hide_password i").removeClass("fa-eye");
+  } else if ($("#show_hide_password input").attr("type") == "password") {
+    $("#show_hide_password input").attr("type", "text");
+    $("#show_hide_password i").removeClass("fa-eye-slash");
+    $("#show_hide_password i").addClass("fa-eye");
+  }
 });
 
-var removeText = $("<div>X</div>");
-removeText.on("click", function() {
-    var current = $(this);
-    current.prev().val("");
-});
+var removeText = $("<div class='removeContent'>X</div>");
+removeText
 
-$("input:text").on("keypress", function() {
-    $(this).next(removeText.clone());
-})
+$("input").on("keypress", function() {
+  var current = $(this);
+  if (current.parent().find(".removeContent").length == 0) {
+    var clone = removeText.clone();
+    clone.on("click", function() {
+      var current = $(this);
+      current.prev().val("");
+      current.remove();
+    });
+    current.after(clone);
+  }
+});
