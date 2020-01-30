241963c7b12773bdca2ad94b51e8c8d5e5dffcc5
pepe_fabra_valverde <jofaval@outlook.com>
2020-01-30 17:13:06 +0100

    Formatted input.js

--- scripts/js/inputs.js
+++ scripts/js/inputs.js
@@ -2,28 +2,28 @@ var typingTimer;
 var usernameInputDoneTypingInterval = 5000;
 var $input = $('#myInput');
 
-$input.on('keyup', function () {
-  clearTimeout(typingTimer);
-  typingTimer = setTimeout(usernameInputDoneTyping, usernameInputDoneTypingInterval);
+$input.on('keyup', function() {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(usernameInputDoneTyping, usernameInputDoneTypingInterval);
 });
 
-$input.on('keydown', function () {
-  clearTimeout(typingTimer);
+$input.on('keydown', function() {
+    clearTimeout(typingTimer);
 });
 
-function usernameInputDoneTyping () {
-  //do something
+function usernameInputDoneTyping() {
+    //do something
 }
 
 $("#show_hide_password a").on('click', function(event) {
     event.preventDefault();
-    if($('#show_hide_password input').attr("type") == "text"){
+    if ($('#show_hide_password input').attr("type") == "text") {
         $('#show_hide_password input').attr('type', 'password');
-        $('#show_hide_password i').addClass( "fa-eye-slash" );
-        $('#show_hide_password i').removeClass( "fa-eye" );
-    }else if($('#show_hide_password input').attr("type") == "password"){
+        $('#show_hide_password i').addClass("fa-eye-slash");
+        $('#show_hide_password i').removeClass("fa-eye");
+    } else if ($('#show_hide_password input').attr("type") == "password") {
         $('#show_hide_password input').attr('type', 'text');
-        $('#show_hide_password i').removeClass( "fa-eye-slash" );
-        $('#show_hide_password i').addClass( "fa-eye" );
+        $('#show_hide_password i').removeClass("fa-eye-slash");
+        $('#show_hide_password i').addClass("fa-eye");
     }
 });
