72c0a27680f843ef26b79e6324eef408b114b5a1
pepe_fabra_valverde <jofaval@outlook.com>
2020-01-31 18:20:19 +0100

    Modify AjaxController to prepare the base

--- libs/AjaxController.php
+++ libs/AjaxController.php
@@ -6,8 +6,16 @@ require_once("exceptions.php");
 if (isset($_REQUEST["operationType"])) {
     $operationType = recoge("operationType");
     try {
+        $params = "";
         switch ($operationType) {
             case 'query':
+                $requiredParams = [
+                    "select", "from"
+                ];
+                if (isset($_REQUEST["where"])) {
+                    $requiredParams[] = "params";
+                }
+                throwIfExceptionIfDoesntExist($requiredParams);
                 $select = recoge("select");
                 $from = recoge("from");
                 $where = recoge("where");
@@ -18,9 +26,26 @@ if (isset($_REQUEST["operationType"])) {
                 errorAction(-1);
                 break;
         }
-    } catch (\Throwable $th) {
-        //throw $th;
+
+        echo $params;
+    } catch (Error $th) {
+        errorAction(-1);
     }
 } else {
     errorAction(-1);
+}
+
+function throwIfExceptionIfDoesntExist($elems) {
+    foreach ($elems as $elem) {
+        if (isset($_REQUEST[$elem])) {
+            throw new Throwable("$elem doesn't exist");
+        }
+    }
+    
+}
+
+function returnError()
+{
+    $json = json_encode((object) array("error" => true,));
+    echo $json;
 }
