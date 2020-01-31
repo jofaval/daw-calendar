405517a1a098f85bf9709336b1418ce02e384f39
pepe_fabra_valverde <jofaval@outlook.com>
2020-01-31 18:57:00 +0100

    Created access.php templates

---
+++ templates/access.php
@@ -0,0 +1,14 @@
+<!--Page configuration-->
+<?php $optionalCSS = []; ?>
+<?php $optionalScripts = []; ?>
+<?php $title = "Access"; ?>
+<?php $showFooter = true; ?>
+<?php $showHeader = true; ?>
+
+<?php ob_start() ?>
+
+<h3> You don't have enough permissions to do this operation. </h3>
+
+<?php $contenido = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
