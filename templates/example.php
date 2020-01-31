47813df7f2ce0972e360240334859ff6e028ee82
pepe_fabra_valverde <jofaval@outlook.com>
2020-01-31 18:33:14 +0100

    Create example test

---
+++ templates/example.php
@@ -0,0 +1,13 @@
+<!--Page configuration-->
+<?php $optionalCSS = ["style.css"]; ?>
+<?php $optionalScripts = ["js/script.js"]; ?>
+<?php $showFooter = true; ?>
+<?php $showHeader = true; ?>
+
+<?php ob_start() ?>
+
+<h3> Hello world! </h3>
+
+<?php $contenido = ob_get_clean() ?>
+
+<?php include 'layout.php' ?>
