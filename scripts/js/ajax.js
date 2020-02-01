bebd3b9e135eca0ba4abf97d1d307a0b799878db
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-02 00:07:09 +0100

    Prepared ajax for month events

--- scripts/js/ajax.js
+++ scripts/js/ajax.js
@@ -13,3 +13,23 @@ function queryDatabase(requestLocation, success, message = "Ha surgido un error 
             sendNotification(message, true);
         });
 }
+
+function getAllEventsFromMonth(month) {
+    request(requestLocation, "POST", {
+        select: "",
+        from: "table",
+        where: "condition",
+        params: [
+            ["key", "value"],
+            ["key", "value"],
+            ["key", "value"],
+        ],
+    }, getAllEventsFromMonth,
+    function() {
+        sendNotification(message, true);
+    });
+}
+
+function getAllEventsFromMonth(data) {
+    
+}
