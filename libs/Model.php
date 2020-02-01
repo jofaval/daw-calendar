94f416f0eff41494660241b528a8a6a198c7a71b
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-01 15:42:00 +0100

    Implemented basic query method to Model

--- libs/Model.php
+++ libs/Model.php
@@ -6,43 +6,31 @@ class Model extends PDO
     protected $conexion;
     public static $instance = null;
 
-    public function __construct()
-    {
-        
-    }
+    public function __construct() {
+        $this->conexion = new PDO('mysql:host=' . Config::$mvc_bd_hostname . ';dbname=' . Config::$mvc_bd_nombre . '', Config::$mvc_bd_usuario, Config::$mvc_bd_clave);
+        // Realiza el enlace con la BD en utf-8
+        $this->conexion->exec("set names utf8");
+        $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     }
 
     public static function getInstance() {
         if (self::$instance == null) {
             self::$instance = new Model();
-            self::$instance->conexion = new PDO('mysql:host=' . Config::$mvc_bd_hostname . ';dbname=' . Config::$mvc_bd_nombre . '', Config::$mvc_bd_usuario, Config::$mvc_bd_clave);
-            // Realiza el enlace con la BD en utf-8
-            self::$instance->conexion->exec("set names utf8");
-            self::$instance->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
     }
 
-    public function dameAlimentos()
+    private function query($queryString, $params = [])
     {
-        $consulta = "select * from alimentos order by energia desc";
-        $result = $this->conexion->query($consulta);
+        $result = $this->conexion->query($queryString);
+
+        if (!empty($params)) {
+            foreach ($params as $key => $value) {
+                $result->bindParam(":$key", $value);
+            }
+        }
+
         return $result->fetchAll();
-    }
-    
-    
-    public function insertarAlimento($n, $e, $p, $hc, $f, $g)
-    {
-        $consulta = "insert into alimentos (nombre, energia, proteina, hidratocarbono, fibra, grasatotal) values (?, ?, ?, ?, ?, ?)";
-        $result = $this->conexion->prepare($consulta);
-        $result->bindParam(1, $n);
-        $result->bindParam(2, $e);
-        $result->bindParam(3, $p);
-        $result->bindParam(4, $hc);
-        $result->bindParam(5, $f);
-        $result->bindParam(6, $g);
-        $result->execute();
-                
-        return $result;
     }
 
     public function isTokenValid($token) {
