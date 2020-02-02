2fb12633ea7dd8f8be794d24d18fd405394cb780
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-02 18:08:49 +0100

    Change crypt salt to something personalized

--- libs/bCrypt.php
+++ libs/bCrypt.php
@@ -1,7 +1,7 @@
 <?php
 function blowfishCrypt($toEncrypt, $key) {
     // Salt starting with $2a$. The two digit cost parameter: 09. 22 characters
-    $saltHash = '$2a$09$anexamplestringforsalt' . $key . '$';
+    $saltHash = '$2a$09$esternocleidomastoideo' . $key . '$';
     return crypt($toEncrypt, $saltHash);
 }
 ?>
