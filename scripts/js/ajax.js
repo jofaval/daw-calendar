2c8470b07fcb928953eaea92c44a2552bec92146
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-03 11:28:55 +0100

    Moved AJAX functions to ajax.js from utils.js

--- scripts/js/ajax.js
+++ scripts/js/ajax.js
@@ -1,3 +1,34 @@
+function request(requestLocation, requestType = "POST", params = {}, success = defaultAjaxSuccessAction, error = defaultAjaxErrorAction, async = false) {
+    $.ajax({
+        url: requestLocation,
+        data: params,
+        type: requestType,
+        async: async,
+        success: success,
+        error: error,
+    });
+}
+
+function defaultAjaxSuccessAction(data) {
+}
+
+function defaultAjaxErrorAction(data) {
+    sendNotification("Ha surgido un error al realizar la operación", true);
+}
+
+function printDateWithFormat(givenDate, format = "d/m/Y") {
+    format = format.replace("y", givenDate.getYear());
+    format = format.replace("Y", givenDate.getFullYear());
+
+    format = format = format.toLowerCase();
+
+    format = format.replace("d", minNumberOfDigits(givenDate.getUTCDate()));
+
+    format = format.replace("m", minNumberOfDigits(givenDate.getMonth() + 1));
+
+    return format;
+}
+
 function queryDatabase(requestLocation, success, message = "Ha surgido un error al realizar la operación") {
     request(requestLocation, "POST", {
             select: "",
@@ -12,24 +43,4 @@ function queryDatabase(requestLocation, success, message = "Ha surgido un error 
         function() {
             sendNotification(message, true);
         });
-}
-
-function getAllEventsFromMonth(month) {
-    request(requestLocation, "POST", {
-        select: "",
-        from: "table",
-        where: "condition",
-        params: [
-            ["key", "value"],
-            ["key", "value"],
-            ["key", "value"],
-        ],
-    }, getAllEventsFromMonth,
-    function() {
-        sendNotification(message, true);
-    });
-}
-
-function getAllEventsFromMonth(data) {
-    
 }
--- scripts/js/utils.js
+++ scripts/js/utils.js
@@ -1,34 +1,3 @@
-function request(requestLocation, requestType = "POST", params = {}, success = defaultAjaxSuccessAction, error = defaultAjaxErrorAction, async = false) {
-    $.ajax({
-        url: requestLocation,
-        data: params,
-        type: requestType,
-        async: async,
-        success: success,
-        error: error,
-    });
-}
-
-function defaultAjaxSuccessAction(data) {
-}
-
-function defaultAjaxErrorAction(data) {
-    sendNotification("Ha surgido un error al realizar la operación", true);
-}
-
-function printDateWithFormat(givenDate, format = "d/m/Y") {
-    format = format.replace("y", givenDate.getYear());
-    format = format.replace("Y", givenDate.getFullYear());
-
-    format = format = format.toLowerCase();
-
-    format = format.replace("d", minNumberOfDigits(givenDate.getUTCDate()));
-
-    format = format.replace("m", minNumberOfDigits(givenDate.getMonth() + 1));
-
-    return format;
-}
-
 function minNumberOfDigits(number, numberOfDigits = 2) {
     return number.toLocaleString("es", {
         minimumIntegerDigits: numberOfDigits,
