345d9f3b48821cd6b5b950999482c90af261253c
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-03 17:00:46 +0100

    Fix Model

--- libs/Model.php
+++ libs/Model.php
@@ -1,28 +1,31 @@
 <?php
-include_once ('Config.php');
-include_once ('bCrypt.php');
-include_once ('bDate.php');
+include_once('Config.php');
+include_once('bCrypt.php');
+include_once('bDate.php');
 
 class Model extends PDO
 {
     protected $conexion;
     public static $instance = null;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->conexion = new PDO('mysql:host=' . Config::$mvc_bd_hostname . ';dbname=' . Config::$mvc_bd_nombre . '', Config::$mvc_bd_usuario, Config::$mvc_bd_clave);
         // Realiza el enlace con la BD en utf-8
         $this->conexion->exec("set names utf8");
         $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     }
+    }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (self::$instance == null) {
             self::$instance = new Model();
         }
         return self::$instance;
     }
 
-    public function query($queryString, $params = []) {
+    public function query($queryString, $params = [])
+    {
         $result = $this->conexion->query($queryString);
 
         if (!empty($params)) {
@@ -34,7 +37,8 @@ class Model extends PDO
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function cudOperation($insertString, $params = []) {
+    public function cudOperation($insertString, $params = [])
+    {
         $result = $this->conexion->query($insertString);
 
         if (!empty($params)) {
@@ -46,46 +50,50 @@ class Model extends PDO
         return $result->execute();
     }
 
-    public function disable($entityType, $params, $enabled) {
+    public function disable($entityType, $params, $enabled)
+    {
         $params = [];
         $params["enabled"] = $enabled;
-        $identification = array_keys()[0];
+        $identification = array_keys($params)[0];
         return $this->cudOperation("UPDATE FROM $entityType SET enabled=:enabled WHERE $identification=:$identification", $params);
     }
 
-    public function signin($username) {
+    public function signin($username)
+    {
         $params = [];
         $params["username"] = $username;
         $signin = $this->query("SELECT access, password FROM users WHERE username=:username", $params);
         return $signin;
     }
 
-    public function signup($username, $password, $fullname, $email) {
+    public function signup($username, $password, $fullname, $email)
+    {
         $params = [
             "username" => $username
         ];
-        if (count($this->query("SELECT username FROM users WHERE username=:username"), ) !== 0) {
+        if (count($this->query("SELECT username FROM users WHERE username=:username"),) !== 0) {
             $params["password"] = blowfishCrypt($password, $username);
             $params["fullname"] = $fullname;
             $params["email"] = $email;
             $signUp = $this->cudOperation("INSERT INTO FROM users (username, password, fullname, email, type) VALUES (:username, :password, :fullname, :email, 2)", $params);
-            generateToken($username);
+            $this->generateToken($username);
             return $signUp;
         }
         return false;
     }
 
-    public function generateToken($username) {
+    public function generateToken($username)
+    {
         $token = "";
 
         do {
             $token = generateRandomKey();
-        }while(count($this->query("SELECT token FROM tokens WHERE token=:token and username=:username"), ["token"=>$token, "username"=>$username]) !== 0);
+        } while (count($this->query("SELECT token FROM tokens WHERE token=:token and username=:username"), ["token" => $token, "username" => $username]) !== 0);
 
         $params = [
             "token" => $token,
             "username" => $username,
-            "expirationDate" => addDays(date(), 2),
+            "expirationDate" => addDays(date("now"), 2),
             "isTraded" => false,
         ];
 
@@ -94,8 +102,9 @@ class Model extends PDO
         return $token;
     }
 
-    public function isTokenValid($username, $token) {
-        $queryResult = $this->query("SELECT expirationDate FROM tokens WHERE token=:token and username=:username", ["token"=>$token, "username"=>$username]);
+    public function isTokenValid($username, $token)
+    {
+        $queryResult = $this->query("SELECT expirationDate FROM tokens WHERE token=:token and username=:username", ["token" => $token, "username" => $username]);
         if (count($queryResult) !== 0) {
             return isDateInTime($queryResult[0]["expirationDate"]);
         }
@@ -103,9 +112,10 @@ class Model extends PDO
         return false;
     }
 
-    public function updateTeacher() {
+    public function updateTeacher()
+    {
         $username = recoge("username");
-        
+
         $params = [
             "name" => recoge("inputName"),
             "email" => recoge("inputEmail"),
@@ -116,7 +126,8 @@ class Model extends PDO
         return $this->cudOperation("UPDATE FROM users SET name=:name, username=:username, password=:password WHERE email=:email type=2", $params);
     }
 
-    public function deleteTeacher() {
+    public function deleteTeacher()
+    {
         $params = [
             "email" => recoge("inputEmail"),
         ];
@@ -124,7 +135,10 @@ class Model extends PDO
         return $this->cudOperation("DELETE FROM users WHERE email=:email", $params);
     }
 
-    public function createClassroom() {
+    public function createClassroom()
+    {
+        $description = recoge("inputClasroomDescription");
+        $state = recoge("selectClasroomState");
         $params = [
             "name" => recoge("inputClassroomName"),
             "description" => recoge("inputClasroomDescription"),
@@ -142,7 +156,8 @@ class Model extends PDO
         return false;
     }
 
-    public function updateClassroom() {
+    public function updateClassroom()
+    {
         $params = [
             "name" => recoge("inputClassroomName"),
             "description" => recoge("inputClasroomDescription"),
@@ -152,7 +167,8 @@ class Model extends PDO
         return $this->cudOperation("UPDATE FROM classrooms SET name=:name, description=:description, state=:state WHERE name=:name", $params);
     }
 
-    public function deleteClassroom() {
+    public function deleteClassroom()
+    {
         $params = [
             "name" => recoge("inputClassroomName"),
         ];
@@ -160,7 +176,10 @@ class Model extends PDO
         return $this->cudOperation("DELETE FROM classrooms WHERE name=:name", $params);
     }
 
-    public function createSchedule() {
+    public function createSchedule()
+    {
+        $description = recoge("inputClasroomDescription");
+        $state = recoge("selectClasroomState");
         $params = [
             "name" => recoge("inputClassroomName"),
             "description" => recoge("inputClasroomDescription"),
@@ -178,7 +197,8 @@ class Model extends PDO
         return false;
     }
 
-    public function updateSchedule() {
+    public function updateSchedule()
+    {
         $params = [
             "name" => recoge("inputClassroomName"),
             "description" => recoge("inputClasroomDescription"),
@@ -188,7 +208,8 @@ class Model extends PDO
         return $this->cudOperation("UPDATE FROM classrooms SET name=:name, description=:description, state=:state WHERE name=:name", $params);
     }
 
-    public function deleteSchedule() {
+    public function deleteSchedule()
+    {
         $params = [
             "name" => recoge("inputClassroomName"),
         ];
@@ -196,29 +217,34 @@ class Model extends PDO
         return $this->cudOperation("DELETE FROM classrooms WHERE name=:name", $params);
     }
 
-    public function getEventsFromMonth() {
+    public function getEventsFromMonth()
+    {
         $params = [
             "month" => recoge("month"),
             "year" => recoge("year"),
         ];
 
-       return $this->query("SELECT * FROM horario_seleccionado WHERE MONTH(fecha_seleccionada)=:month and YEAR(fecha_seleccionada)=:year", $params);
+        return $this->query("SELECT * FROM horario_seleccionado WHERE MONTH(fecha_seleccionada)=:month and YEAR(fecha_seleccionada)=:year", $params);
     }
 
-    public function getTeachers() {
-       return $this->query("SELECT * FROM users WHERE type=2 and enabled=true");
+    public function getTeachers()
+    {
+        return $this->query("SELECT * FROM users WHERE type=2 and enabled=true");
     }
 
-    public function getClassrooms() {
-       return $this->query("SELECT * FROM classrooms WHERE enabled=true");
+    public function getClassrooms()
+    {
+        return $this->query("SELECT * FROM classrooms WHERE enabled=true");
     }
 
-    public function getSchedules() {
-        $params = ["year" => getAcademicYear(date())];
-       return $this->query("SELECT * FROM schedules WHERE enabled=true and YEAR(year)=:year", $params);
+    public function getSchedules()
+    {
+        $params = ["year" => getAcademicYear(date("now"))];
+        return $this->query("SELECT * FROM schedules WHERE enabled=true and YEAR(year)=:year", $params);
     }
 
-    public function createEvent() {
+    public function createEvent()
+    {
         $sessions = Sessions::getInstance();
 
         $params = [
@@ -231,11 +257,12 @@ class Model extends PDO
         if (count($this->query("SELECT name FROM events WHERE startHour=:startHour and date=:date", $params)) === 0) {
             return $this->query("INSERT INTO FROM events (title, startHour, date, username) WHERE startHour=:startHour and date=:date", $params);
         }
-        
+
         return false;
     }
 
-    public function updateEvent() {
+    public function updateEvent()
+    {
         $params = [
             "title" => recoge("title"),
             "startHour" => recoge("startHour"),
@@ -245,12 +272,13 @@ class Model extends PDO
         return $this->query("UPDATE FROM events SET title=:title WHERE startHour=:startHour and date=:date", $params);
     }
 
-    public function deleteEvent() {
+    public function deleteEvent()
+    {
         $params = [
             "startHour" => recoge("startHour"),
             "date" => recoge("date"),
         ];
 
-       return $this->query("DELETE FROM events WHERE startHour=:startHour and date=:date", $params);
+        return $this->query("DELETE FROM events WHERE startHour=:startHour and date=:date", $params);
     }
 }
