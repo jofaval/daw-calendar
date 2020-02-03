b65b7141ac1c56b7b612d3cc9864a28f7648e2e8
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-03 14:33:26 +0100

    Changed Model in Calendar

--- scripts/js/MVCCalendar.js
+++ scripts/js/MVCCalendar.js
@@ -9,58 +9,18 @@
 class Model {
     constructor() {
         this.currentEvents = [];
+        this.currentDate = new Date();
+        getEventsFromMonth(this.currentDate.getMonth(), this.currentDate.getFullYear(), function (data) {
+            this.currentEvents = data;
+        });
+    }
+
+    addEvent(title, startHour, date) {
         
     }
 
-    bindTodoListChanged(callback) {
-        this.onTodoListChanged = callback
-    }
-
-    _commit(todos) {
-        this.onTodoListChanged(todos)
-        localStorage.setItem('todos', JSON.stringify(todos))
-    }
-
-    addTodo(todoText) {
-        const todo = {
-            id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
-            text: todoText,
-            complete: false,
-        }
-
-        this.todos.push(todo)
-
-        this._commit(this.todos)
-    }
-
-    editTodo(id, updatedText) {
-        this.todos = this.todos.map(todo =>
-            todo.id === id ? {
-                id: todo.id,
-                text: updatedText,
-                complete: todo.complete
-            } : todo
-        )
-
-        this._commit(this.todos)
-    }
-
-    deleteTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id)
-
-        this._commit(this.todos)
-    }
-
-    toggleTodo(id) {
-        this.todos = this.todos.map(todo =>
-            todo.id === id ? {
-                id: todo.id,
-                text: todo.text,
-                complete: !todo.complete
-            } : todo
-        )
-
-        this._commit(this.todos)
+    removeEvent(startHour, date) {
+        
     }
 }
 
