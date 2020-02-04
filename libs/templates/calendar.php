619e25da1952d51d12027295654dae6dc7116c01
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-04 19:26:33 +0100

    Change Calendar with MVCCalendar.js implemented

--- libs/templates/calendar.php
+++ libs/templates/calendar.php
@@ -1,49 +1,13 @@
 <!--Page configuration-->
 <?php $optionalCSS = ["mini-event-calendar.min.css", "timetable.css"]; ?>
-<?php $optionalScripts = ["libs/time-table.js", "libs/mini-event-calendar.js", "js/calendar-controls.js", "js/calendar.js",]; ?>
+<?php $optionalScripts = ["libs/time-table.js", "libs/mini-event-calendar.js", "js/calendar-controls.js", "js/MVCCalendar.js",]; ?>
 <?php $title = "Booking area"; ?>
 <?php $mainClasses = ""; ?>
-<?php $showFooter = false; ?>
-<?php $showHeader = false; ?>
+<?php $showFooter = true; ?>
+<?php $showHeader = true; ?>
 
 <?php ob_start() ?>
 
-<form class="form-signin">
-    <div class="text-center mb-4 text-white">
-        <h3>LogIn</h3>
-    </div>
+<?php $contenido = ob_get_clean(); ?>
 
-    <div class="form-label-group">
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" aria-describedby="emailHelp" required="" autofocus="">
-        <label for="inputEmail">Email address</label>
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-            else.</small>
-    </div>
-
-    <br>
-
-    <div class="form-label-group d-flex justify-content-center align-items-center" id="show_hide_password">
-        <input type="password" class="form-control col-md" id="inputPassword" placeholder="Password">
-        <label for="inputPassword">Password</label>
-        <a href="" class="input-group-addon password-hide col-md-1 bg-primary d-flex justify-content-center align-items-center rounded">
-            <i class="fa fa-eye-slash text-white" aria-hidden="true"></i>
-        </a>
-    </div>
-
-    <div class="checkbox mb-3 text-white text-center">
-        <label>
-            <input type="checkbox" value="remember-me"> Remember me
-        </label> &nbsp;|&nbsp;
-        <a href="">
-            Forgot password?
-        </a> &nbsp;|&nbsp;
-        <a href="">
-            You registered but can't login?
-        </a>
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-</form>
-
-<?php $contenido = ob_get_clean() ?>
-
-<?php include_once'layout.php' ?>
+<?php include_once 'layout.php' ?>
