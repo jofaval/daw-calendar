879f4785e6d4a3b02df7f1602173259247095cda
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-04 19:29:13 +0100

    Fixed calendar external sources

--- libs/templates/calendar.php
+++ libs/templates/calendar.php
@@ -1,6 +1,12 @@
 <!--Page configuration-->
-<?php $optionalCSS = ["mini-event-calendar.min.css", "timetable.css"]; ?>
-<?php $optionalScripts = ["libs/time-table.js", "libs/mini-event-calendar.js", "js/calendar-controls.js", "js/MVCCalendar.js",]; ?>
+<?php $optionalCSS = ["mini-event-calendar.min.css", "timetable.css", "login.css"]; ?>
+<?php $optionalScripts = [
+    "js/AjaxController.js",
+    "libs/time-table.js",
+    "libs/mini-event-calendar.js",
+    "js/calendar-controls.js",
+    "js/MVCCalendar.js",
+]; ?>
 <?php $title = "Booking area"; ?>
 <?php $mainClasses = ""; ?>
 <?php $showFooter = true; ?>
