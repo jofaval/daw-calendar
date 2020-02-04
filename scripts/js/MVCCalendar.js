f76d5db318d82520bbf2aa4dada0da2ff3d3fdb1
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-04 01:15:39 +0100

    Add base View in Calendar

--- scripts/js/MVCCalendar.js
+++ scripts/js/MVCCalendar.js
@@ -10,7 +10,7 @@ class Model {
     constructor() {
         this.currentEvents = [];
         this.currentDate = new Date();
-        getEventsFromMonth(this.currentDate.getMonth(), this.currentDate.getFullYear(), function(data) {
+        getEventsFromMonth(this.currentDate.getMonth(), this.currentDate.getFullYear(), function (data) {
             this.currentEvents = data;
         });
         this.instace = this;
@@ -18,7 +18,7 @@ class Model {
 
     addEvent(title, startHour, date, success) {
         var instance = this.instance;
-        createEvent(title, startHour, date, function(data) {
+        createEvent(title, startHour, date, function (data) {
             instance.currentEvents.push({
                 "title": title,
                 "startHour": startHour,
@@ -30,7 +30,7 @@ class Model {
 
     removeEvent(startHour, date, success) {
         var instance = this.instance;
-        removeEvent(title, startHour, date, function(data) {
+        removeEvent(title, startHour, date, function (data) {
             instance.currentEvents = data;
             success(data);
         });
@@ -38,7 +38,7 @@ class Model {
 
     updateEvent(title, startHour, date, success) {
         var instance = this.instance;
-        updateEvent(title, startHour, date, function(data) {
+        updateEvent(title, startHour, date, function (data) {
             instance.currentEvents = data;
             instance.currentEvents.forEach(element => {
                 element.startHour
@@ -49,15 +49,15 @@ class Model {
 
     getEventsFromWeek(date, days) {
         var instance = this.instance;
-        getEventsFromWeek(date, days, function(data) {
+        getEventsFromWeek(date, days, function (data) {
             instance.currentEvents = data;
             success(data);
         });
     }
-    
+
     getEventsFromDay(date, days) {
         var instance = this.instance;
-        getEventsFromDay(date, days, function(data) {
+        getEventsFromDay(date, days, function (data) {
             instance.currentEvents = data;
             success(data);
         });
@@ -71,144 +71,24 @@ class Model {
  */
 class View {
     constructor() {
-        this.app = $('root');
-        this.form = this.createElement('form')
-        this.input = this.createElement('input')
-        this.input.type = 'text'
-        this.input.placeholder = 'Add todo'
-        this.input.name = 'todo'
-        this.submitButton = this.createElement('button')
-        this.submitButton.textContent = 'Submit'
-        this.form.append(this.input, this.submitButton)
-        this.title = this.createElement('h1')
-        this.title.textContent = 'Todos'
-        this.todoList = this.createElement('ul', 'todo-list')
-        this.app.append(this.title, this.form, this.todoList)
+        this.mainContainer = $('main');
 
-        this._temporaryTodoText = ''
-        this._initLocalListeners()
-    }
+        //Weekly calendar
+        this.weeklyCalendarContainer = $('<section class="container col-md-10"></section>');
+        var weeklyRow = $("<div class='row'></div");
+        this.timeTableWeek = $('<div id="timeTableWeek" class="col-md row"></div>');
+        weeklyRow.append(this.timeTableWeek);
+        this.weeklyCalendarContainer.append(weeklyRow);
 
-    get _todoText() {
-        return this.input.value
-    }
+        //Monthly calendar
+        this.monthlyCalendarContainer = $('<section class="container col-md-10"></section>');
+        var monthlyRow = $("<div class='row'></div");
+        this.monthCalendar = $('<div id="calendar" class="col-md-8"></div>');
+        this.timeTableDay = $('<div id="timeTableWeek" class="col-md row"></div>');
+        this.monthlyRow.append(this.monthCalendar, this.timeTableDay);
+        this.monthlyCalendarContainer.append(monthlyRow);
 
-    _resetInput() {
-        this.input.value = ''
-    }
-
-    createElement(tag, className) {
-        const element = document.createElement(tag)
-
-        if (className) element.classList.add(className)
-
-        return element
-    }
-
-    getElement(selector) {
-        const element = document.querySelector(selector)
-
-        return element
-    }
-
-    displayTodos(todos) {
-        // Delete all nodes
-        while (this.todoList.firstChild) {
-            this.todoList.removeChild(this.todoList.firstChild)
-        }
-
-        // Show default message
-        if (todos.length === 0) {
-            const p = this.createElement('p')
-            p.textContent = 'Nothing to do! Add a task?'
-            this.todoList.append(p)
-        } else {
-            // Create nodes
-            todos.forEach(todo => {
-                const li = this.createElement('li')
-                li.id = todo.id
-
-                const checkbox = this.createElement('input')
-                checkbox.type = 'checkbox'
-                checkbox.checked = todo.complete
-
-                const span = this.createElement('span')
-                span.contentEditable = true
-                span.classList.add('editable')
-
-                if (todo.complete) {
-                    const strike = this.createElement('s')
-                    strike.textContent = todo.text
-                    span.append(strike)
-                } else {
-                    span.textContent = todo.text
-                }
-
-                const deleteButton = this.createElement('button', 'delete')
-                deleteButton.textContent = 'Delete'
-                li.append(checkbox, span, deleteButton)
-
-                // Append nodes
-                this.todoList.append(li)
-            })
-        }
-
-        // Debugging
-        console.log(todos)
-    }
-
-    _initLocalListeners() {
-        this.todoList.addEventListener('input', event => {
-            if (event.target.className === 'editable') {
-                this._temporaryTodoText = event.target.innerText
-            }
-        })
-    }
-
-    bindAddTodo(handler) {
-        this.form.addEventListener('submit', event => {
-            event.preventDefault()
-
-            if (this._todoText) {
-                handler(this._todoText)
-                this._resetInput()
-            }
-        })
-    }
-
-    bindDeleteTodo(handler) {
-        this.todoList.addEventListener('click', event => {
-            if (event.target.className === 'delete') {
-                const id = parseInt(event.target.parentElement.id)
-
-                handler(id)
-            }
-        })
-    }
-
-    bindEditTodo(handler) {
-        this.todoList.addEventListener('focusout', event => {
-            if (this._temporaryTodoText) {
-                const id = parseInt(event.target.parentElement.id)
-
-                handler(id, this._temporaryTodoText)
-                this._temporaryTodoText = ''
-            }
-        })
-    }
-
-    bindToggleTodo(handler) {
-        this.todoList.addEventListener('change', event => {
-            if (event.target.type === 'checkbox') {
-                const id = parseInt(event.target.parentElement.id)
-
-                handler(id)
-            }
-        })
-    }
-
-    createElement(tag, className = "") {
-        return $(`<${tag} class="${className}"></${tag}>`);
+        this.mainContainer.append(this.weeklyCalendar, this.monthlyCalendar);
     }
 }
 
@@ -222,8 +102,8 @@ class View {
  */
 class Controller {
     constructor(model, view) {
-        this.model = model
-        this.view = view
+        this.model = model;
+        this.view = view;
 
         // Explicit this binding
         this.model.bindTodoListChanged(this.onTodoListChanged)
@@ -235,7 +115,7 @@ class Controller {
         // Display initial todos
         this.onTodoListChanged(this.model.todos)
     }
-    
+
 
 }
 
