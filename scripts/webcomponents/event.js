8191de4016b80e61e9c9869052b2746a57a952e4
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-05 22:07:45 +0100

    Modify event webcomponent with event type

--- scripts/webcomponents/event.js
+++ scripts/webcomponents/event.js
@@ -8,6 +8,24 @@ class Event extends HTMLElement {
 
         this.shadowRoot.innerHTML = `
         <link rel="stylesheet" href="../styles/bootstrap.min.css">
+        <style>
+        .card-icon {
+            z-index: 5;
+            cursor: pointer;
+        }
+
+        .card-icon:hover {
+            background: rgba(0, 0, 0, 0.25) !important;
+        }
+
+        .card.free {
+            background: rgb(63, 140, 255) !important;
+        }
+
+        .card.yours {
+            background: rgb(255, 178, 63) !important;
+        }
+        </style>
             <div class="card mb-3 mr-3 bg-dark">
                 <span
                     class="pull-right w-100 position-absolute p-1 d-flex flex-row-reverse clickable close-icon">
@@ -19,23 +37,32 @@ class Event extends HTMLElement {
                 <div class="row no-gutters text-light">
                     <div
                         class="col-3 col-sm d-flex justify-content-center text-center align-content-center flex-column">
-                        <div class="align-middle" id="eventStartHour">` + this.getAttribute("event-start-hour") + `</div>
+                        <div class="align-middle" id="eventStartHour">${this.getAttribute("event-start-hour")}</div>
                         <div class="align-middle">-</div>
-                        <div class="align-middle" id="eventEndHour">` + this.getAttribute("event-end-hour") + `</div>
+                        <div class="align-middle" id="eventEndHour">${this.getAttribute("event-end-hour")}</div>
                     </div>
                     <div class="col-md-10 col-9 px-0">
                         <div class="card-body">
-                            <p class="card-title">` + this.getAttribute("event-title") + `</p>
+                            <p class="card-title">${this.getAttribute("event-title")}</p>
                             <p class="card-text"><small class="text-light">Booked by
-                                    <a id="teacherEmail" href="mailto:` + this.getAttribute("teacher-email") + `"
-                                        class="text-warning"><b id="teacherName">` + this.getAttribute("teacher-name") + `</b></a></small>
+                                    <a id="teacherEmail" href="mailto:${this.getAttribute("teacher-email")}"
+                                        class="text-warning"><b id="teacherName">${this.getAttribute("teacher-name")}</b></a></small>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
         `;
-        console.log($(this).find(".card-icon"));
+        var eventActions = this.shadowRoot.querySelectorAll(".card-icon");
+        var close = eventActions[0];
+        var event = this;
+        close.addEventListener("click", function () {
+            event.remove();
+        });
+        var edit = eventActions[1];
+        edit.addEventListener("click", function () {
+            event.remove();
+        });
 
     }
 
@@ -57,10 +84,13 @@ class Event extends HTMLElement {
             case "teacher-name":
                 this.shadowRoot.querySelector("#teacherEmail").setAttribute("href", "mailto:" + newValue);
                 break;
+            case "event-type":
+                this.shadowRoot.querySelector(".card").className = "card mb-3 mr-3 bg-dark " + newValue;
+                break;
         }
     }
     static get observedAttributes() {
-        return ["event-start-hour", "event-end-hour", "event-title", "teacher-email", "teacher-name"];
+        return ["event-start-hour", "event-end-hour", "event-title", "teacher-email", "teacher-name", "event-type"];
     }
 }
 
