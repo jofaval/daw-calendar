b08c8e5efd1315c9056e3601e82fc3a7e5ca80b2
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-06 01:27:57 +0100

    Update access template

--- libs/templates/access.php
+++ libs/templates/access.php
@@ -10,7 +10,18 @@
 
 <?php ob_start()?>
 
-<h3> You don't have enough permissions to do this operation. </h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="access-template text-center text-light">
+                <h1>
+                    You don't have access!</h1>
+                <h2>
+                    It seems you don't have enough permissions for that action</h2>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php $contenido = ob_get_clean()?>
 
