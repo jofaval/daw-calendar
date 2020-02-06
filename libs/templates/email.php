a9bb916b2af628f2a7f3f71286930fd2bcbe2854
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-06 01:30:09 +0100

    Update email confirmation template

--- libs/templates/email.php
+++ libs/templates/email.php
@@ -10,13 +10,21 @@
 
 <?php ob_start()?>
 
-<?php if ($params["isInTime"]): ?>
-<form action="" method="POST">
-    <button class="btn btn-primary btn-lg">Confirmar email</button>
-</form>
-<?php else: ?>
-<h3>The validation token already expired, we're sorry.</h3>
-<?php endif;?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="access-template text-center text-light">
+                <?php if ($params["isInTime"]): ?>
+                <form action="" method="POST">
+                    <button class="btn btn-primary btn-lg">Confirmar email</button>
+                </form>
+                <?php else: ?>
+                <h3>The validation token already expired, we're sorry.</h3>
+                <?php endif;?>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php $contenido = ob_get_clean()?>
 
