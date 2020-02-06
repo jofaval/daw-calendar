0d0e06f778296b17060e3b6621296d45ef974274
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-06 01:28:50 +0100

    Update not signed template

--- libs/templates/notsigned.php
+++ libs/templates/notsigned.php
@@ -10,7 +10,17 @@
 
 <?php ob_start()?>
 
-<h3> You're not signed in, <a href="index.php?ctl=signin">signin</a>. </h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="notsigned-template text-center text-light">
+                <h1>
+                    You're not signin!</h1>
+                <h2>Please <a href="index.php?ctl=signin">signin</a>. </h2>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php $contenido = ob_get_clean()?>
 
