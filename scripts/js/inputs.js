567cc1ba90d5d336d1279c34c0923d78f7bfd546
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-06 10:43:50 +0100

    Add password strength checker

--- scripts/js/inputs.js
+++ scripts/js/inputs.js
@@ -34,6 +34,27 @@ $("#show_hide_password .trigger").on("click", function (event) {
   return false;
 });
 
+var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+var mediumRegex = new RegExp(
+  "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+$("#inputPassword").on("keyup", function () {
+  var current = $(this);
+
+  current.removeClass("border-success");
+  current.removeClass("border-warning");
+  current.removeClass("border-danger");
+
+  var value = current.val();
+  if (strongRegex.test(value)) {
+    current.addClass("border-success");
+  } else if (mediumRegex.test(value)) {
+    current.addClass("border-warning");
+  } else {
+    current.addClass("border-danger");
+  }
+});
+
 /*var removeText = $("<div class='removeContent'>X</div>");
 
 $("input").on("keypress", function () {
