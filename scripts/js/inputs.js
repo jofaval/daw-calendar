96afd5e7cf0dffc6ca177a068f880be5250f0f3d
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-06 15:19:52 +0100

    Format input.js

--- scripts/js/inputs.js
+++ scripts/js/inputs.js
@@ -2,74 +2,74 @@ var typingTimer;
 var usernameInputDoneTypingInterval = 5000;
 var $input = $("#myInput");
 
-$input.on("keyup", function () {
-  clearTimeout(typingTimer);
-  typingTimer = setTimeout(
-    usernameInputDoneTyping,
-    usernameInputDoneTypingInterval
-  );
+$input.on("keyup", function() {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(
+        usernameInputDoneTyping,
+        usernameInputDoneTypingInterval
+    );
 });
 
-$input.on("keydown", function () {
-  clearTimeout(typingTimer);
+$input.on("keydown", function() {
+    clearTimeout(typingTimer);
 });
 
 function usernameInputDoneTyping() {
-  //do something
+    //do something
 }
 
-$("#show_hide_password .trigger").on("click", function (event) {
-  var event = event || window.event;
-  event.preventDefault();
-  if ($("#show_hide_password input").attr("type") == "text") {
-    $("#show_hide_password input").attr("type", "password");
-    $("#show_hide_password i").addClass("fa-eye-slash");
-    $("#show_hide_password i").removeClass("fa-eye");
-  } else if ($("#show_hide_password input").attr("type") == "password") {
-    $("#show_hide_password input").attr("type", "text");
-    $("#show_hide_password i").removeClass("fa-eye-slash");
-    $("#show_hide_password i").addClass("fa-eye");
-  }
+$("#show_hide_password .trigger").on("click", function(event) {
+    var event = event || window.event;
+    event.preventDefault();
+    if ($("#show_hide_password input").attr("type") == "text") {
+        $("#show_hide_password input").attr("type", "password");
+        $("#show_hide_password i").addClass("fa-eye-slash");
+        $("#show_hide_password i").removeClass("fa-eye");
+    } else if ($("#show_hide_password input").attr("type") == "password") {
+        $("#show_hide_password input").attr("type", "text");
+        $("#show_hide_password i").removeClass("fa-eye-slash");
+        $("#show_hide_password i").addClass("fa-eye");
+    }
 
-  return false;
+    return false;
 });
 
 var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 var mediumRegex = new RegExp(
-  "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
 
-$("#inputPassword").on("keyup", function () {
-  var current = $(this);
+$("#inputPassword").on("keyup", function() {
+    var current = $(this);
 
-  current.removeClass("border-success");
-  current.removeClass("border-warning");
-  current.removeClass("border-danger");
+    current.removeClass("border-success");
+    current.removeClass("border-warning");
+    current.removeClass("border-danger");
 
-  var value = current.val();
-  if (strongRegex.test(value)) {
-    current.addClass("border-success");
-  } else if (mediumRegex.test(value)) {
-    current.addClass("border-warning");
-  } else {
-    current.addClass("border-danger");
-  }
+    var value = current.val();
+    if (strongRegex.test(value)) {
+        current.addClass("border-success");
+    } else if (mediumRegex.test(value)) {
+        current.addClass("border-warning");
+    } else {
+        current.addClass("border-danger");
+    }
 });
 
-$("input:file").change(function () {
-  var fullPath = $(this).val();
+$("input:file").change(function() {
+    var fullPath = $(this).val();
 
-  var filename = "Choose file";
+    var filename = "Choose file";
 
-  if (fullPath.length > 0) {
-    var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf(
-      '/'));
-    filename = fullPath.substring(startIndex);
-    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
-      filename = filename.substring(1);
+    if (fullPath.length > 0) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf(
+            '/'));
+        filename = fullPath.substring(startIndex);
+        if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+            filename = filename.substring(1);
+        }
     }
-  }
 
-  $(this).next().html(filename);
+    $(this).next().html(filename);
 });
 
 /*var removeText = $("<div class='removeContent'>X</div>");
