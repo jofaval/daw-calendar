df94f07756a782deb4677f0eab9a0f7d9e715f1f
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-07 17:39:12 +0100

    Added AjaxController admin crud

--- classes/AjaxController.php
+++ classes/AjaxController.php
@@ -1,7 +1,9 @@
 <?php
 
-class AjaxController {
-    public function genericAjaxReturn($functionName, $requiredParams = []) {
+class AjaxController
+{
+    public function genericAjaxReturn($functionName, $requiredParams = [])
+    {
         try {
             if (!empty($requiredParams)) {
                 $this->throwIfExceptionIfDoesntExist($requiredParams);
@@ -21,60 +23,113 @@ class AjaxController {
         }
     }
 
-    public function throwIfExceptionIfDoesntExist($elems) {
+    public function throwIfExceptionIfDoesntExist($elems)
+    {
         foreach ($elems as $elem) {
             if (isset($_REQUEST[$elem])) {
                 throw new Throwable("$elem doesn't exist");
             }
         }
     }
-    
-    public function returnError() {
+
+    public function returnError()
+    {
         $json = json_encode((object) array("error" => true,));
         echo $json;
     }
 
-    public function getEventsFromMonth() {
+    public function getEventsFromMonth()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["month", "year"]);
     }
 
-    public function getTeachers() {
+    public function getTeachers()
+    {
         $this->genericAjaxReturn(__FUNCTION__);
     }
 
-    public function getClassrooms() {
+    public function getClassrooms()
+    {
         $this->genericAjaxReturn(__FUNCTION__);
     }
 
-    public function getSchedules() {
+    public function getSchedules()
+    {
         $this->genericAjaxReturn(__FUNCTION__);
     }
 
-    public function createEvent() {
+    public function createEvent()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["title", "startHour", "date"]);
     }
-    
-    public function updateEvent() {
+
+    public function updateEvent()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["title", "startHour", "date"]);
     }
-    
-    public function deleteEvent() {
+
+    public function deleteEvent()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["startHour", "date"]);
     }
-    
-    public function getSchedule() {
+
+    public function getSchedule()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["selectedYear"]);
     }
-    
-    public function getEventsFromDay() {
+
+    public function getEventsFromDay()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["selectedDay"]);
     }
 
-    public function getEventsFromWeek() {
+    public function getEventsFromWeek()
+    {
         $this->genericAjaxReturn(__FUNCTION__, ["startingDate", "endingDate"]);
     }
 
-    public function test() {
+    public function test()
+    {
         $this->genericAjaxReturn(__FUNCTION__);
+    }
+
+    public function updateTeacher()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputTeacherUsername", "inputTeacherPassword", "inputTeacherName", "inputTeacherEmail"]);
+    }
+    
+    public function deleteTeacher()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputTeacherEmail"]);
+    }
+
+    public function createClassroom()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputClassroomName", "inputClasroomDescription", "selectClasroomState"]);
+    }
+
+    public function updateClassroom()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputClassroomName", "inputClasroomDescription", "selectClasroomState"]);
+    }
+
+    public function deleteClassroom()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputClassroomName"]);
+    }
+
+    public function createSchedule()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputScheduleStartHour", "inputScheduleEndHour", "year"]);
+    }
+
+    public function updateSchedule()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputScheduleStartHour", "inputScheduleEndHour", "year"]);
+    }
+
+    public function deleteSchedule()
+    {
+        $this->genericAjaxReturn(__FUNCTION__, ["inputScheduleStartHour", "year"]);
     }
 }
