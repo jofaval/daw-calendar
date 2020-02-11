9b6ec2528e3cddb65e85c751a2f21f36a5ecc7fd
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-11 02:07:36 +0100

    Add classroom event

--- server/templates/classrooms.php
+++ server/templates/classrooms.php
@@ -1,26 +1,21 @@
 <!--Page configuration-->
-<?php $optionalCSS = []; ?>
-<?php $optionalScripts = []; ?>
-<?php $title = "Classroom selector"; ?>
-<?php $mainClasses = ""; ?>
-<?php $showFooter = true; ?>
-<?php $showHeader = true; ?>
-<?php $showBreadcrumb = false; ?>
-<?php $breadcrumb = []; ?>
+<?php $optionalCSS = [];?>
+<?php $optionalScripts = ["webcomponents/classroom.js"];?>
+<?php $title = "Classroom selector";?>
+<?php $mainClasses = "";?>
+<?php $showFooter = true;?>
+<?php $showHeader = true;?>
+<?php $showBreadcrumb = false;?>
+<?php $breadcrumb = [];?>
 
-<?php ob_start() ?>
+<?php ob_start()?>
 
-<?php $classrooms = Model::getInstance()->query("SELECT * FROM classrooms WHERE enabled!=0"); ?>
-<?php foreach ($classrooms as $row) : ?>
-<div class="card m-3">
-    <div class="card-body">
-        <h5 class="card-title text-center"><?php echo $row["name"]; ?></h5>
-        <p class="card-text"><?php echo $row["description"]; ?></p>
-        <a href="index.php?ctl=calendar&classroom=<?php echo $row["name"]; ?>" class="btn btn-primary w-100">Go to calendar</a>
-    </div>
-</div>
-<?php endforeach; ?>
+<?php $classrooms = Model::getInstance()->query("SELECT * FROM classrooms WHERE enabled!=0");?>
+<?php foreach ($classrooms as $row): ?>
+<classroom-card classroom-name="<?php echo $row["name"]; ?>" classroom-description="<?php echo $row["description"]; ?>">
+</classroom-card>
+<?php endforeach;?>
 
-<?php $contenido = ob_get_clean(); ?>
+<?php $contenido = ob_get_clean();?>
 
-<?php include_once 'layout.php' ?>
+<?php include_once 'layout.php'?>
