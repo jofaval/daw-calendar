949b4af54ed327cdc56c331183d83cb694818991
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-13 02:25:46 +0100

    Add details Modal for side

--- client/scripts/js/Modal.js
+++ client/scripts/js/Modal.js
@@ -1,5 +1,5 @@
 class Modal {
-    static genericModalWithForm(formName) {
+    static genericModalWithForm(formName, readonly = false) {
         $.ajax({
             url: "../index.php?ctl=getForm" + formName,
             type: "POST",
@@ -9,6 +9,10 @@ class Modal {
                     content: data,
                     theme: $.sweetModal.THEME_DARK
                 });
+                if (readonly) {
+                    $("form *[type=submit]").remove();
+                    $("form input").attr("readonly", true);
+                }
             },
             error: function () {
                 Modal.modalError("An error occured, couldn't load form");
