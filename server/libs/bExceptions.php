a9e895e27e505dd1014804a53bdda46c2de5896d
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-15 16:11:50 +0100

    Add extra debugging elements to bExceptions

--- server/libs/bExceptions.php
+++ server/libs/bExceptions.php
@@ -9,21 +9,24 @@ class ExceptionUtils
             } else {
                 header("Location: ./index.php?ctl=error");
             }
-        } catch (Exception $e) {
-            $fw = fopen(__DIR__ . "/../server/logs/logException.txt" . "a+");
-            fwrite($fw, $e->getMessage() . microtime() . PHP_EOL);
-            fclose($fw);
-            header("Location: ./index.php?ctl=error");
+        } catch (Exception $exception) {
+            writeToLog("Exception", "Exception happend at " . microtime() . " with message (" . $exception->getMessage() . ")." . PHP_EOL);
+            //header("Location: ./index.php?ctl=error");
             var_dump($e);
         } catch (Error $e) {
-            $fw = fopen(__DIR__ . "/../server/logs/logError.txt" . "a+");
-            fwrite($fw, $e->getMessage() . microtime() . PHP_EOL);
-            fclose($fw);
-            header("Location: ./index.php?ctl=error");
+            writeToLog("Error", "Error with number " . ($errno) . " happend at " . microtime() . " with message (" . ($e->$errstr) . ") inside \"" . ($e->$errfile) . "\" file at line " . ($e . $errline) . "." . PHP_EOL);
+            //header("Location: ./index.php?ctl=error");
         }
 
         return null;
     }
+}
+
+function writeToLog($log, $message)
+{
+    $fw = fopen(__DIR__ . "/../server/logs/log$log.txt" . "a+");
+    fwrite($fw, $message);
+    fclose($fw);
 }
 
 if (!Config::$developmentMode) {
@@ -33,13 +36,12 @@ if (!Config::$developmentMode) {
 
 function expcetionHandler($exception)
 {
-    error_log("Exception happend at " . microtime() . " with message (" . $exception->getMessage() . ")." . PHP_EOL, 3, __DIR__ . "/../server/logs/logError.txt");
-    header("Location: ./index.php?ctl=error");
+    writeToLog("Exception", "Exception happend at " . microtime() . " with message (" . $exception->getMessage() . ")." . PHP_EOL);
+    //header("Location: ./index.php?ctl=error");
 }
 
 function errorAction($errno = -1, $errstr = "", $errfile = "", $errline = 0)
 {
-    //error_log("Error with number " . ($e->$errno) . " happend at " . microtime() . " with message (" . ($e->$errstr) . ") inside \"" . ($e->$errfile) . "\" file at line " . ($e . $errline) . "." . PHP_EOL, 3, "./logs/logError.txt");
-    error_log("Error with number $errno happend at " . microtime() . " with message ($errstr) inside \"$errfile\" file at line $errline." . PHP_EOL, 3, __DIR__ . "/../server/logs/logError.txt");
-    header("Location: ./index.php?ctl=error");
+    writeToLog("Error", "Error with number " . ($errno) . " happend at " . microtime() . " with message (" . ($errstr) . ") inside \"" . ($errfile) . "\" file at line " . ($errline) . "." . PHP_EOL);
+    //header("Location: ./index.php?ctl=error");
 }
