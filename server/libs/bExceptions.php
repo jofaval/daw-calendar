d9a71e1ad66a80fd8bcde0ba17da95c2dac38946
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-15 10:18:30 +0100

    Fix bExceptions

--- server/libs/bExceptions.php
+++ server/libs/bExceptions.php
@@ -10,10 +10,10 @@ class ExceptionUtils
                 header("Location: ./index.php?ctl=error");
             }
         } catch (Exception $e) {
-            error_log($e->getMessage() . microtime() . PHP_EOL, 3, "./logs/logException.txt");
+            error_log($e->getMessage() . microtime() . PHP_EOL, 3, __DIR__ . "/../server/logs/logException.txt");
             header("Location: ./index.php?ctl=error");
         } catch (Error $e) {
-            error_log($e->getMessage() . microtime() . PHP_EOL, 3, "./logs/logError.txt");
+            error_log($e->getMessage() . microtime() . PHP_EOL, 3, __DIR__ . "/../server/logs/logError.txt");
             header("Location: ./index.php?ctl=error");
         }
 
@@ -28,13 +28,13 @@ if (!Config::$developmentMode) {
 
 function expcetionHandler($exception)
 {
-    error_log("Exception happend at " . microtime() . " with message (" . $exception->getMessage() . ")." . PHP_EOL, 3, "./logs/logError.txt");
+    error_log("Exception happend at " . microtime() . " with message (" . $exception->getMessage() . ")." . PHP_EOL, 3, __DIR__ . "/../server/logs/logError.txt");
     header("Location: ./index.php?ctl=error");
 }
 
 function errorAction($errno = -1, $errstr = "", $errfile = "", $errline = 0)
 {
     //error_log("Error with number " . ($e->$errno) . " happend at " . microtime() . " with message (" . ($e->$errstr) . ") inside \"" . ($e->$errfile) . "\" file at line " . ($e . $errline) . "." . PHP_EOL, 3, "./logs/logError.txt");
-    error_log("Error with number $errno happend at " . microtime() . " with message ($errstr) inside \"$errfile\" file at line $errline." . PHP_EOL, 3, "./logs/logError.txt");
+    error_log("Error with number $errno happend at " . microtime() . " with message ($errstr) inside \"$errfile\" file at line $errline." . PHP_EOL, 3, __DIR__ . "/../server/logs/logError.txt");
     header("Location: ./index.php?ctl=error");
 }
