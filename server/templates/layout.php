ef88d011b5eba4492732353eb0d394682ddc355d
pepe_fabra_valverde <jofaval@outlook.com>
2020-02-16 22:21:05 +0100

    Remove calendar from layout

--- server/templates/layout.php
+++ server/templates/layout.php
@@ -47,10 +47,6 @@
                     <a class="nav-link selected" href="index.php?ctl=classrooms"
                         style="transition: all 0.2s ease-in-out 0s;">Classrooms</a>
                 </li>
-                <li class="nav-item" style="transition: all 0.2s ease-in-out 0s;">
-                    <a class="nav-link selected" href="index.php?ctl=calendar"
-                        style="transition: all 0.2s ease-in-out 0s;">Calendar</a>
-                </li>
                 <?php if ($sessions->getSession("access") >= 2): ?>
                 <li class="nav-item" style="transition: all 0.2s ease-in-out 0s;">
                     <a class="nav-link selected" href="index.php?ctl=admin"
